ba1d2611c9168b768b904a9827eacfa7f675ffcb
sirthebenjamin <126521851+sirthebenjamin@users.noreply.github.com>
2023-03-11 13:14:14 -0500

    adds myfile.md

--- test/indexTest.js
+++ test/indexTest.js
@@ -10,8 +10,8 @@ describe('index.js', function () {
   describe('Array functions', function () {
     beforeEach(function () {
       cats.length = 0;
-
-      cats.push('Milo', 'Otis', 'Garfield');
+    
+      cats.push ('Milo', 'Otis', 'Garfield');
     });
 
     describe('destructivelyAppendCat(name)', function () {
@@ -71,7 +71,7 @@ describe('index.js', function () {
     });
 
     describe('removeFirstCat()', function () {
-      it('removes the first cat from the cats array and returns a new array, leaving the cats array unchanged', function () {
+      it('removes the first cat from the  cats array and returns a new array, leaving the cats array unchanged', function () {
         expect(removeFirstCat()).to.have.ordered.members(["Otis", "Garfield"]);
 
         expect(cats).to.have.ordered.members(["Milo", "Otis", "Garfield"]);
